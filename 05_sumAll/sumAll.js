73be65cd8ff0373aa92bcbfb18350036abd186d0
Jeremiah Viray <jeremiahvirayofficial@gmail.com>
2023-04-14 12:48:58 -0700

    Solution for Odin Project  sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+// Implement a function that takes 2 integers and returns the sum of every number between
+// 1. Create a counter variable
+// 2.
 
+const sumAll = function (num1, num2) {
+  let sum = 0;
+  if (
+    typeof num1 === "number" &&
+    typeof num2 === "number" &&
+    num1 >= 0 &&
+    num2 >= 0
+  ) {
+    for (
+      let i = Number(`${num1 < num2 ? num1 : num2}`);
+      i <= Number(`${num1 < num2 ? num2 : num1}`);
+      i++
+    ) {
+      sum += i;
+    }
+    return sum;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
